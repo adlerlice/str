3efd681e26084bbb24ffbd2d1a65b2d5b15bdeca
adlerlice <adlerlice@gmail.com>
2020-06-03 05:54:57 +0700

    1.1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include "file.h"
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	n = CountBooks();  //Считаем, сколько книжек у нас в файле
+
+	Book* BM = new Book[n]; //Создаём динамический массив под кол-во книг. Массив будет содержать в себе книги в виде структур
+	FillBooks(BM, n);       //Заполняем массив данными из файла
+
+	
+
+	return 0;
+}
+
